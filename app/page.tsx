7e21da72fbd24cb40f962920af37f78be7221ca0
Vadzim_Hardzenia <vadimgordenya@gmail.com>
2024-05-04 16:04:27 +0300

    feat: Implemented base layout for the Marketing page

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import {Button} from "../components/ui/button";
-
-export default function Home() {
-  return (
-    <div>
-      <Button size='lg' className="text-green-500 font-bold">Hello Duolingo</Button>
-    </div>
-  );
-}
